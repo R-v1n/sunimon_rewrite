ba1fa5c10b0ea44c287d8da861d80fcbf854a2fe
R-v1n <76698141+R-v1n@users.noreply.github.com>
2021-01-15 11:30:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ bot.load_extension("cogs.help")
 bot.load_extension("cogs.suni_pic")
 bot.load_extension("cogs.cflip")
 bot.load_extension("cogs.candb")
-#bot.load_extension("cogs.dr_mini")
+bot.load_extension("cogs.dr_mini")
 bot.load_extension("cogs.ytsearch")
 token="enter your token here "
 bot.run(token)
