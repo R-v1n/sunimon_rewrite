8c3fa2ee145b4edfa16a37c92728bcfc89a4e4d6
R-v1n <76698141+R-v1n@users.noreply.github.com>
2021-02-03 03:58:38 +0530

    Update suni_pic.py

--- cogs/suni_pic.py
+++ cogs/suni_pic.py
@@ -5,7 +5,7 @@ class suni_pic(commands.Cog):
         self.bot=bot
     @commands.command()
     async def sunipic(self,ctx):
-        await ctx.send(file=discord.File("/home/kali/win_share/sunimon_rewrite/files/sunimon.png"))
+        await ctx.send(file=discord.File("{file_path}/files/sunimon.png"))
 
 def setup(bot):
     bot.add_cog(suni_pic(bot))
